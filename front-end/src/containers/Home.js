12e2c8fbc4e2f62159c523c9473c774564d5c64a
rafaelMartins1922 <rafael.martins@icomp.ufam.edu.br>
2021-01-05 19:40:59 -0300

    Starting with the front-end

---
+++ front-end/src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Minion's Market</h1>
+        <p className="text-muted">A simple minion toys app</p>
+      </div>
+    </div>
+  );
+}
